33f7dbdf70e068b7a5ac7d46522d8f5ed7c909cb
ganeshparajuli <ganeshparajuli199@gmail.com>
2022-05-18 22:51:36 +0530

    initial setup postcss tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/./index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "./styles/tailwind.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
